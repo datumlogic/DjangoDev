62b3a94aecd0e11acef7a8bf810c697ab462577d
Gene Myers <code@datumlogic.com>
2013-06-04 14:05:20 +0100

    another change to DjangoDev.com

--- DjangoDev.cmd
+++ DjangoDev.cmd
@@ -1,6 +1,6 @@
 D:
 cd \DjangoDev
 del *.pyc /s
-del DB/Survey.db /s
+del ./DB/Survey.db /s
 git pull
-copy /V /Y DjangoDev.cmd C:\Users\Administrator\* 
+copy /V /Y DjangoDev.cmd C:\Users\Administrator\
